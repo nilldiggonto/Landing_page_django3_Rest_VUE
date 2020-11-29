0d3888b79fd2cf3250d01138a8c57f56ee5affb9
nilldiggonto <nilldiggonto@gmail.com>
2020-11-29 20:44:42 +0600

    Like Dislike API

--- questions/api/urls.py
+++ questions/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.urls.conf import include
 from rest_framework.routers import DefaultRouter
-from questions.api.views import QuestionViewSet,AnswerCreateAPIView,QuestionAnswerListAPIView
+from questions.api.views import (QuestionViewSet,AnswerCreateAPIView,
+                                    QuestionAnswerListAPIView,AnswerRUDAPIView,AnswerLikeAPIView,)
 
 
 router = DefaultRouter()
@@ -12,4 +13,6 @@ urlpatterns = [
     path('',include(router.urls)),
     path('questions/<slug:slug>/answer/list/',QuestionAnswerListAPIView.as_view(),name='answer-list'),
     path('questions/<slug:slug>/answer/',AnswerCreateAPIView.as_view(),name='answer'),
+    path('answer/<int:pk>/',AnswerRUDAPIView.as_view(),name='answer-detail'),
+    path('answer/<int:pk>/like/',AnswerLikeAPIView.as_view(),name='answer-like'),
 ]
--- questions/api/views.py
+++ questions/api/views.py
@@ -1,14 +1,17 @@
 from django.http import request
-from rest_framework import viewsets,generics
+from rest_framework import viewsets,generics,status
+from rest_framework import serializers
 from rest_framework.generics import get_object_or_404
 from rest_framework.exceptions import ValidationError
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from questions.api.serializers import QuestionSerializer,AnswerSerializer
 from questions.models import Question,Answer
 from questions.api.permissions import IsAuthOrReadOnly
 
 
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 
 #For Question
@@ -48,3 +51,38 @@ class QuestionAnswerListAPIView(generics.ListAPIView):
         kwarg_slug = self.kwargs.get('slug')
         return Answer.objects.filter(question__slug = kwarg_slug).order_by('-created_at')
         
+
+### Detail and Like
+class AnswerRUDAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+    permission_classes = [IsAuthenticated,IsAuthOrReadOnly]
+
+
+#like api
+class AnswerLikeAPIView(APIView):
+    serializer_class = AnswerSerializer
+    permission_classes = [IsAuthenticated]
+
+    def delete(self,request,pk):
+        answer = get_object_or_404(Answer,pk=pk)
+        user = request.user
+        answer.voters.remove(user)
+        answer.save()
+
+        serializer_context = {'request':request}
+        serializer = self.serializer_class(answer,context = serializer_context)
+
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+
+    def post(self,request,pk):
+        answer = get_object_or_404(Answer,pk=pk)
+        user = request.user
+        answer.voters.add(user)
+        answer.save()
+
+        serializer_context = {'request':request}
+        serializer = self.serializer_class(answer,context = serializer_context)
+
+        return Response(serializer.data,status=status.HTTP_200_OK)
