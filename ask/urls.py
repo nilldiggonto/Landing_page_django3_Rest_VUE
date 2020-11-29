21414b4c58dd52719a71aaf6e0212e945a14e1f7
nilldiggonto <nilldiggonto@gmail.com>
2020-11-29 11:43:21 +0600

    Core Template

--- ask/urls.py
+++ ask/urls.py
@@ -1,16 +1,19 @@
 
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 
 from users.forms import CustomUserForm
 from django_registration.backends.one_step.views import RegistrationView
+from core.views import IndexTemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/',include('django_registration.backends.one_step.urls')),
-
     path('accounts/registration/',RegistrationView.as_view(form_class=CustomUserForm, success_url='/'),
                                         name = 'django_registration_register'),
+    path('accounts/',include('django_registration.backends.one_step.urls')),
+    path('accounts/',include('django.contrib.auth.urls')),
+
+    
 
     
 
@@ -18,4 +21,7 @@ urlpatterns = [
     path('api/rest-auth/',include('rest_auth.urls')),
 
     path('api/rest-auth/registration/',include('rest_auth.registration.urls')),
+
+    # re_path(r"^.*$",IndexTemplateView.as_view(),name='core_point'),
+    path('',IndexTemplateView.as_view(),name='core-pint'),
 ]
