fccddf8dccc41483342663fa532fb251018d3a6d
nilldiggonto <nilldiggonto@gmail.com>
2020-12-01 19:44:27 +0600

    homepage

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -2,15 +2,15 @@
 <template>
     <!--Navbar-->
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary" style="padding:0rem !important;">
         <div class="container">
                     
   <!-- <a class="navbar-brand" href="#">Navbar</a>
  -->
   <router-link :to="{name:'Home'}" class="navbar-brand">Ask Anything </router-link>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
-  </button>
+  </button> -->
 
   <div class="collapse navbar-collapse" id="navbarColor01">
     <ul class="navbar-nav ml-auto">
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,20 +1,56 @@
 <template>
   <div class="home">
     <div class="container">
-      <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+      <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+
+    <div class="card mt-2" v-for="question in questions" :key="question.pk">
+    
+      <div class="card-body">
+        <blockquote class="blockquote text-left">
+        <p class="mb-0">{{question.content}}</p>
+        <footer class="blockquote-footer">@{{question.author}} |
+           <strong>
+             <span class="text-info">Answer: {{question.answer_count}}</span>
+           </strong>
+           </footer>
+         
+      </blockquote>
+      </div>
+    </div>
+
+
     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+
+//API Servie
+import {apiService} from '../common/api.service'
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  data(){
+    return {
+      questions: []
+    }
+  },
+  methods:{
+    getQuestion(){
+      let endpoint = 'api/q/questions/';
+      apiService(endpoint).then(data=>{
+        this.questions.push(...data.results);
+      })
+    }
+  },
+  created(){
+    this.getQuestion()
+    console.log(this.questions)
   }
+
 };
 </script>
