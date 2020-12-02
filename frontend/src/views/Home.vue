375b818686afbf338bb92a7e9370da10bb1ff878
nilldiggonto <nilldiggonto@gmail.com>
2020-12-02 11:33:20 +0600

    Async Load more

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -4,14 +4,15 @@
       <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
 
-    <div class="card mt-2" v-for="question in questions" :key="question.pk">
+    <div class="card mt-2 mb-2 border-primary" v-for="question in questions" :key="question.pk">
     
       <div class="card-body">
-        <blockquote class="blockquote text-left">
+        <blockquote class="blockquote text-center">
         <!-- <p class="mb-0">{{question.content}}</p> -->
         <router-link style="text-decoration:none;font-size:30px;" class="mb-0" :to="{name:'Question_d',params:{slug: question.slug}}"> 
           {{question.content}}
         </router-link>
+        
         <footer class="blockquote-footer">@{{question.author}} |
            <strong>
              <span class="text-info">Answer: {{question.answer_count}}</span>
@@ -21,6 +22,8 @@
       </blockquote>
       </div>
     </div>
+    <p v-show="loadingQuestions">Loading............</p>
+    <button v-show="next" @click="getQuestion" class="btn btn-primary btn-block">Load More</button>
 
 
     </div>
@@ -39,14 +42,27 @@ export default {
   name: "Home",
   data(){
     return {
-      questions: []
+      questions: [],
+      next: null,
+      loadingQuestions:false
     }
   },
   methods:{
     getQuestion(){
       let endpoint = `/api/q/questions/`;
+      if (this.next){
+        endpoint = this.next;
+      }
+      this.loadingQuestions = true
       apiService(endpoint).then(data=>{
         this.questions.push(...data.results);
+        this.loadingQuestions= false;
+        if (data.next){
+          this.next = data.next;
+        }
+        else{
+          this.next = null;
+        }
       })
     }
   },
