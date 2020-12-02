6a2b7dd3dfb5ae9b20c1dd533654bd924b869118
nilldiggonto <nilldiggonto@gmail.com>
2020-12-02 20:11:09 +0600

    Dev Template

--- ask/settings.py
+++ ask/settings.py
@@ -127,7 +127,7 @@ LOGOUT_REDIRECT_URL = '/'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'frontend/dist'),
     )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
--- core/views.py
+++ core/views.py
@@ -1,8 +1,13 @@
+from django.conf import settings
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 
 class IndexTemplateView(LoginRequiredMixin,TemplateView):
     def get_template_names(self):
-        template_name = 'index.html'
+        if not settings.DEBUG:
+            template_name = 'index.html'
+        else:
+            template_name= 'main.html'
         return template_name
